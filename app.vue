3fd436bbdbc02fa6bb55270224c55d58c1de54f3
Frances Pasulohan <franpas12123@gmail.com>
2023-08-15 15:32:09 +0800

    arrange components in app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container mx-auto">
+    <BaseHeader />
+    <main class="container">
+      <BaseHero />
+      <div class="h-[100vh]"></div>
+      <BaseContactMe />
+      <BaseFooter />
+    </main>
   </div>
 </template>
