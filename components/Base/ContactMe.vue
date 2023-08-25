95f00bd93756e0ed4c8277f293f75063b84a15c8
Frances Pasulohan <franpas12123@gmail.com>
2023-08-25 12:13:26 +0800

    add form and modify style

--- components/Base/ContactMe.vue
+++ components/Base/ContactMe.vue
@@ -1,27 +1,75 @@
 <template>
-  <section id="contact-me" class="grid grid-cols-1 md:grid-cols-2">
-    <div>
-      <p class="text-4xl">Got a project in mind?</p>
+  <section
+    id="contact-me"
+    ref="contactMe"
+    class="mt-20 grid grid-cols-1 px-5 sm:mt-0 sm:px-0 md:grid-cols-2"
+  >
+    <div class="z-10 lg:mx-12">
+      <div class="mb-3 grid grid-cols-2 gap-5">
+        <div class="input-label">
+          <label for="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Juan dela Cruz"
+          />
+        </div>
+        <div class="input-label">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="juandelacruz@company.com"
+          />
+        </div>
+      </div>
+      <div class="input-label">
+        <label for="message">Message</label>
+        <textarea
+          name="message"
+          id="1"
+          cols="30"
+          rows="10"
+          placeholder="Message me"
+        ></textarea>
+      </div>
     </div>
-    <div>
-      <div class="grid grid-cols-2">
-        <div>
-          <label for="name">Your name</label>
-          <input type="text" name="name" id="name" />
-        </div>
-        <div>
-          <label for="email">Your email</label>
-          <input type="email" name="email" id="email" />
-        </div>
-      </div>
-      <div class="grid grid-cols-1">
-        <label for="message">Your message</label>
-        <textarea name="message" id="1" cols="30" rows="10"></textarea>
-      </div>
+    <div class="">
+      <!-- <p class="text-4xl">Got a project in mind?</p> -->
+      <img
+        class="deal mx-auto max-h-[450px]"
+        src="img/partnership.gif"
+        alt=""
+      />
     </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+const form = reactive({
+  name: null,
+  email: null,
+});
+</script>
 
-<style scoped></style>
+<style scoped>
+input,
+textarea {
+  @apply rounded-xl text-black;
+}
+label {
+  @apply pb-1 text-xl;
+}
+.input-label {
+  @apply grid grid-cols-1;
+}
+.deal {
+  /* @apply bg-gradient-to-b from-white via-white to-bg; */
+}
+#contact-me {
+  min-height: 60vh !important;
+  /* background-color: white; */
+}
+</style>
