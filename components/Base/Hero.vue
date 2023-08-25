d007e3c0fecf4bb60eca7eca38c7172cd9b15602
Frances Pasulohan <franpas12123@gmail.com>
2023-08-25 12:14:25 +0800

    add scroll effect onclick and modify style

--- components/Base/Hero.vue
+++ components/Base/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="hero" class="mx-5 h-screen">
+  <section id="hero" class="mx-3 h-screen md:mt-10 md:-mb-10">
     <div class="grid gap-5 sm:h-2/3 sm:grid-cols-5">
       <div
         class="order-last my-auto grid content-end sm:order-1 sm:col-span-2 sm:pt-10"
@@ -15,10 +15,11 @@
         <p class="mt-5 text-center font-extralight sm:text-end">
           {{ description }}
         </p>
-        <div class="mx-auto mt-5 sm:ml-auto">
+        <div class="mx-auto mt-5 sm:ml-auto sm:mr-0">
           <button
             type="button"
-            class="rounded-xl bg-cyan-500 p-3 px-5 text-xl font-medium text-white shadow-lg shadow-cyan-600/50 hover:bg-cyan-400 hover:shadow-cyan-500/50"
+            class="rounded-xl bg-cyan-500 p-3 px-5 text-xl font-medium text-white shadow"
+            @click="handleClick"
           >
             {{ ctaText }}
           </button>
@@ -32,11 +33,22 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from "vue";
 const name = "Frances Pasulohan";
 const position = "full-stack developer";
 const description =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, eveniet quo ipsam magnam eius explicabo expedita quis natus aperiam?";
 const ctaText = "Contact Me";
+
+onMounted(() => {
+  // contactMe.scrollIntoView({ behavior: "smooth" });
+});
+const handleClick = () => {
+  window.scrollTo(0, 9999);
+  // console.log("ref", ref.);
+  // const el =
+  // ref.scrollIntoView({ behavior: "smooth" });
+};
 </script>
 
 <style scoped></style>
